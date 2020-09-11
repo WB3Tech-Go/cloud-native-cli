dbff13c0f5135dc5fa676af33067ff37af5a226c
Bill Bensing <billbensing@wb3tech.com>
2020-09-10 20:04:43 -0400

    removed commit statement:

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,6 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		println("~~~~ Hello CLI ~~~~")
 	},
 }
 
